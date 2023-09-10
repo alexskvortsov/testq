eefc883eb08324494d401b8493019e89437a523e
alexskvortsov <sky0210@yandex.ru>
2023-09-10 11:07:14 +0300

    add new func1

--- test3.h
+++ test3.h
@@ -1,12 +1,8 @@
-#include <stdio.h>
+//#include <stdio.h>
 
 int foo(int a, int b)
 {
-return a+b:
-
+return a+b;
+}
 int foo(int a, int b, int c, int d)
-{rerurn a+b+c+d;
-
-}
-  
-}
+{return a+b+c+d;}
--- test4.h
+++ test4.h
@@ -1,1 +1,8 @@
 int foo(int a, int b, int c);
+
+int fioo(int a, int b, int c);
+
+int fiii( int a);
+
+int fiii( int a);
+int fiii( int a);
