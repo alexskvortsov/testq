ebcacdb8894fbf9256c2a30531d9f25ad47da133
alexskvortsov <sky0210@yandex.ru>
2023-09-10 10:55:28 +0300

    add1

--- test1.h
+++ test1.h
@@ -2,5 +2,10 @@
 #define _TEST1_H
 
 int test1fun(int a, int b);
+int tetctdfd;
+int fdfsdf;
+
+int foo(int a, int b. int c);
+
 
 #endif
--- test2.h
+++ test2.h
@@ -2,5 +2,8 @@
 #define _TEST2_H
 
 int test2fun(int a, int b);
+int test3fung(int a, int b);
+
+int fun4(int a, int b, int c);
 
 #endif
