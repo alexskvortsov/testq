967117b5b3d3dacc57986fce3252a2d0c7d07755
Alex <35403410+alexskvortsov@users.noreply.github.com>
2023-09-10 10:43:57 +0300

    Create main.c
    main file

---
+++ main.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include "test1.h"
+//#include "test2.h"
+
+
+
+int main(int arrg, char**argv)
+{
+	
+	printf("Hello world\n\r");
+	
+	int a = 10;
+	int b = 5;
+	int num1 = 0, num2 = 0;
+	
+	num1 = test1fun(a,b);
+	num2 = test2fun(a,b);
+	
+	printf("%d + %d = %d\n\r", a, b, num1);
+	printf("%d - %d = %d\n\r", a, b, num2);
+	
+	int num3 = num1-num2;
+	
+
+
+	return 0;
+}
